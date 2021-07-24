93f4ae2a31b3fd7cbc9ebc36284dbea67bcb8e06
Clement Wahaga <c.wahaga@cci-formation.nc>
2021-07-24 05:34:39 +0200

    add modal

--- resources/views/medecin.blade.php
+++ resources/views/medecin.blade.php
@@ -13,31 +13,70 @@
 </head>
 <body>
 <h1>liste des medecins</h1>
-<a href="{{('/medecinform')}}" class="btn btn-primary my-3">+ medecin</a>
+<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#change_password">+ medecin</button>
 
 
 
-<ul>
-            <table class="table">
-                <thead>
-                <tr>
+<div class="card" style="width: 18rem;">
+           
+    <table class="table table-striped">
+        <thead>
+                 <tr>
                     <th scope="col">#</th>
                     <th scope="col">Nom</th>
                     <th scope="col">prenom</th>
                     <th scope="col">specialité</th>
                 </tr>
-                </thead>
-                <tbody>
-                    @foreach($medecin as $data)
+        </thead>
+        <tbody >
+            @foreach($medecin as $data)
                 <tr>
-                    <th scope="row">{{ $data->id }}</th>
-                    <td>{{ $data->nom }}</a></td>
-                    <td>{{ $data->prenom }}</td>
-                    <td>{{ $data->specialite }}</td>
-                </tr>
-                    @endforeach
-                </tbody>
-            </table>
-</ul>
+                    <th>{{ $data->id }}</th>
+                    <th>{{ $data->nom }}</th>
+                    <th>{{ $data->prenom }}</th>
+                    <th>{{ $data->specialite }}</th>
+                    <th class="actions">
+                        <a href="#" class="edit btn btn-primary my-3  btn-lg">modifier</a>
+                        <a href="#" class="destroy btn btn-danger my-3 btn-lg">supprimer</a>
+                    </th> 
+	            </tr>
+            @endforeach               
+        </tbody>
+    </table>
+</div class="card">
+
+<div class="modal fade" id="change_password" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title">ajouter un medecin</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                            <div class="modal-body">
+                                <form action="{{ route('medecin.create') }}" method="POST">
+                                    <div class="mb-1 col-8 ">
+                                        <label for="" class="form-label">Nom du medecin</label>
+                                        <input type="text" class="form-control" id="nom" aria-describedby="emailHelp">
+                                    </div>
+                                    <div class="mb-3  col-8">
+                                        <label for="" class="form-label">Prenom du medecin </label>
+                                        <input type="text" class="form-control" id="prenom">
+                                    </div>
+                                    <div class="mb-3  col-8">
+                                        <label for="" class="form-label">specialite </label>
+                                        <input type="text" class="form-control" id="specialite">
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Envoyé</button>
+                                </form>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-danger" data-dismiss="modal">Fermer</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            
 </body>
 </html>
